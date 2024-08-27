8b545e039314da342f42393a617cc815621f2d33
Nayaknaina <nainanayak288@.com>
2024-08-27 14:41:08 +0530

    gethelp

--- src/app.js
+++ src/app.js
@@ -222,6 +222,12 @@ app.get("/profile", isAdminLoggedIn, async (req, res) => {
     res.render("profile", { user });
 });
 
+app.get("/gethelp", isAdminLoggedIn, async (req, res) => {
+
+  const user = await logIncollection.findById(req.user.id);
+
+  res.render("gethelp", { user });
+});
 
   
 
