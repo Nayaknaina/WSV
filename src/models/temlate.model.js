8fec9486427539afd80983bb6574b5d3302f8409
mr. Karan <karanghorse91@gmail.com>
2024-08-30 18:46:31 +0530

    templete file

---
+++ src/models/temlate.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const templateSchema = new mongoose.Schema(
+  {
+    title:{
+      type:String,
+      lowercase: true,
+      trim: true,
+    },              
+    text:{
+      type:String,
+      lowercase: true,
+      trim: true,
+    },
+    uid: {
+      type: mongoose.Schema.ObjectId,
+      ref: "logIncollection",
+    },
+    cid:{
+      type: String,
+    },
+    image:{
+      type: String,
+    },
+    pdf:{
+      type: String,
+    },
+    client: {
+      type: Boolean,
+      default: false
+    },
+    team: {
+      type: Boolean,
+      default: false
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("template", templateSchema);
