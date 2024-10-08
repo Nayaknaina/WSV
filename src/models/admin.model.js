93af5e4c3d6e8f46cca2703196c4c6a2e5417aad
Dev <karanghorse91@gmail.com>
2024-10-08 17:59:49 +0530

    manual lead sending whatsapp message only change in sendLeadFun , manualLead, addRemark, inviteTeammember

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -6,119 +6,133 @@ const logInSchema = new mongoose.Schema({
   googleId: {
     type: String,
     unique: true,
-    sparse: true, 
+    sparse: true,
   },
-  cid:{
+  cid: {
     type: String,
   },
-  facebookToken:{
-    type:String,
+  facebookToken: {
+    type: String,
   },
-  whatsappSession:{
-    type:String,
+  whatsappSession: {
+    type: String,
   },
   fcmToken: String,
-  paymentProof:{
+  paymentProof: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Payment',
+    ref: "Payment",
   },
-  teams: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'teamMember',
-  }],
+  teams: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "teamMember",
+    },
+  ],
 
-  myPipelines: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'pipeline',
-  }],
+  myPipelines: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "pipeline",
+    },
+  ],
 
-  myleads:[{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'lead'
-  }],
+  myleads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "lead",
+    },
+  ],
 
-  myTemplates:[{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'template'
-  }],
-
+  myTemplates: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "template",
+    },
+  ],
 
   name: {
     type: String,
     trim: true,
-    max: 24
+    max: 24,
   },
   email: {
     type: String,
     required: true,
     unique: true,
     trim: true,
-    lowercase: true
+    lowercase: true,
   },
-  mobile:{
-    type:String,
+  mobile: {
+    type: String,
     trim: true,
   },
   profilePicture: {
     type: String,
-    trim: true, 
-   
+    trim: true,
   },
   password: {
     type: String,
-    required: function() { return !this.googleId; } 
+    required: function () {
+      return !this.googleId;
+    },
   },
-  address:{
-    type:String,
+  address: {
+    type: String,
     lowercase: true,
     trim: true,
   },
-  city:{
-    type:String,
+  city: {
+    type: String,
     lowercase: true,
     trim: true,
   },
-  state:{
-    type:String,
+  state: {
+    type: String,
     lowercase: true,
     trim: true,
   },
-  countryCode:{
-    type:String,
+  countryCode: {
+    type: String,
     lowercase: true,
-    trim: true
+    trim: true,
   },
   organizationName: { type: String },
-  sector: { type: String, },
+  sector: { type: String },
 
   // Add a new field to track subscription level
   subscriptionLevel: {
     type: String,
-    enum: ['free', 'basic', 'premium'],
-    default: 'free'
+    enum: ["free", "basic", "premium"],
+    default: "free",
   },
- 
+  subscriptionExpiry: {
+    type: Date, // Store the expiration date
+  },
 });
 
-logInSchema.pre('save', function (next) {
+logInSchema.pre("save", function (next) {
   const maxTeams = {
     free: 3,
     basic: 7,
-    premium: 1000
+    premium: 1000,
   };
 
   const userSubscriptionLevel = this.subscriptionLevel;
   const maxAllowedTeams = maxTeams[userSubscriptionLevel] || 3; // Default to free
 
   if (this.teams.length > maxAllowedTeams) {
-    const err = new Error(`Cannot add more than ${maxAllowedTeams} team members with ${userSubscriptionLevel} plan.`);
-    next(err);
-  } else {
-    next();
+    const err = new Error(
+      `Cannot add more than ${maxAllowedTeams} team members with ${userSubscriptionLevel} plan.`
+    );
+    return next(err);
   }
+  if (userSubscriptionLevel === "free" && !this.subscriptionExpiry) {
+    this.subscriptionExpiry = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000); // 15 days from now
+  }
+  next();
 });
 
 // Define the model
-const logIncollection = mongoose.model('logIncollection', logInSchema);
+const logIncollection = mongoose.model("logIncollection", logInSchema);
 
 module.exports = logIncollection;
